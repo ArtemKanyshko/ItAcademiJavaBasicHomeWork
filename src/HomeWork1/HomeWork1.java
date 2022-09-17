d5e2ed7713f1fc9621b5a5778723bb8e9f90819b
ArtemKanyshko <kanyshkaa@outlook.com>
2022-09-17 11:28:39 +0300

    HomeWork1

---
+++ src/HomeWork1/HomeWork1.java
@@ -0,0 +1,22 @@
+package HomeWork1;
+
+public class HomeWork1 {
+    public static void main(String[] args) {
+        byte b = 0x55;
+        System.out.println("b=" + b);
+        short s = 0x55ff;
+        System.out.println("s=" + s);
+        int i = 1000000;
+        System.out.println("i=" + i);
+        long l = 0xffffffffL;
+        System.out.println("l=" + l);
+        char c = 'a';
+        System.out.println("c=" + c);
+        float f = .25f;
+        System.out.println("f=" + f);
+        double d = .00001234;
+        System.out.println("d=" + d);
+        boolean bool = true;
+        System.out.println("bool=" + bool);
+    }
+}
